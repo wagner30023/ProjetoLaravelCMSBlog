e630ab3a781babb3cb21dec6c36635b7e19f6631
Carlos Wagner <wagner30023@gmail.com>
2021-08-18 17:09:52 -0300

    implementaçao dos contrellers e dashborad Adminlte

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,12 @@
+@extends('adminlte::page')
+
+@section('title','Painel')
+
+@section('content_header')
+    <h1> Painel de controle </h1>
+@endsection
+
+@section('content')
+    ....
+@endsection
+
